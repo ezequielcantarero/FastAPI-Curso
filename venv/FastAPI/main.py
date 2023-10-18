8243afd90493d902a8b0b9ff24af4d31547a8a5f
Ezequiel <ezequielcantarero@gmail.com>
2023-10-18 12:33:46 -0300

    Se agrega la funcion aumento de Porcentaje(es una prueba por fuera del proyecto de FastAPI)

--- venv/FastAPI/main.py
+++ venv/FastAPI/main.py
@@ -9,3 +9,10 @@ app.include_router(productos.router)
 @app.get('/')
 async def read_root():
     return{'Mensaje':'Bienvenidos a la API'}
+
+@app.get('/porcentaje')
+async def aumento_porcentaje(base: float, porcentaje: float ,dias: int):
+    valor_final = base
+    for x in range(dias):
+        valor_final = valor_final + valor_final * (porcentaje / 100)    
+    return valor_final
